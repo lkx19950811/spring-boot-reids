4d459d0f12b1a9697f0604e7e12f9949f25f8997
lkx <lkx19950811@163.com>
2018-10-30 13:27:03 +0800

    新增了 redis 秒杀测试
    (cherry picked from commit be8afa8)

---
+++ src/test/java/com/lkx/springbootjedis/OnlyTest.java
@@ -0,0 +1,29 @@
+package com.lkx.springbootjedis;
+
+import com.lkx.springbootjedis.pojo.HashPojo;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author lee Cather
+ * @date 2018-10-19 11:25
+ * desc :
+ */
+public class OnlyTest {
+    @Test
+    public void con(){
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("a","a");
+        HashPojo o1 = new HashPojo(2);
+        HashPojo o2 = new HashPojo(4);
+        HashMap hashMap = new HashMap();
+        hashMap.put("aaa","aaa");
+        hashMap.put("bbb","bbb");
+        hashMap.put(o1,o1);
+        hashMap.put(o2,o2);
+        Object object= hashMap.get(o1);
+        System.out.println(object);
+    }
+}
